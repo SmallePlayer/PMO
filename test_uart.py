ee20b44d084035605d1841c20324b6977ab4d0c7
KUVSHIN-123 <kirill_gorbachev12@mail.ru>
2024-11-26 15:49:49 +0300

    0.1.7

--- test_uart.py
+++ test_uart.py
@@ -1,13 +1,11 @@
-from PMO import *
 import uart
 import config
 
 def main():
-    ua = PMO(ip_addr=config.host, port=config.port, topic="uart")
     data = [100,0,-98,12]
-    ua.uart_send(data)
+    uart.uart_send(data)
     print(f"Отправляю данные: {data}")
-    response = ua.uart_read()
+    response = uart.uart_read()
     print(f"response: {response}")
 
 if __name__ == "__main__":
