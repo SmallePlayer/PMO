bd914a7bf00092404b191f4cf6bb7dc9a667648c
SmallePlayer <147194611+SmallePlayer@users.noreply.github.com>
2024-11-28 00:25:30 +0300

    0.1.9.1

--- PMO.py
+++ PMO.py
@@ -3,7 +3,7 @@ import time
 import cv2
 import base64
 import numpy as np
-import uart
+
 from publish_pyobj_video import video_potok
 from subscriver_pyobj_video import video_read
 from ultralytics import YOLO
